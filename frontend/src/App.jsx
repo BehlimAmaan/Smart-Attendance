5f763db9e4e0dae93741c10d2676431cb9225576
BehlimAmaan <itzamaanbehlim45@gmail.com>
2026-03-06 13:00:21 +0530

    Added Protected Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import StudentDashboard from "./pages/StudentDashboard";
 import TeacherDashboard from "./pages/TeacherDashboard";
@@ -9,19 +10,72 @@ import EditTeacherProfile from "./pages/EditTeacherProfile";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 
+import ProtectedRoute from "./auth/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/teacher" element={<TeacherDashboard />} />
-        <Route path="/register-face" element={<RegisterFace />} />
-        <Route path="/change-password" element={<ChangePassword />} />
-        <Route path="/add-student" element={<AddStudent />} />
-        <Route path="/teacher/profile" element={<EditTeacherProfile />} />
+
+        <Route
+          path="/student"
+          element={
+            <ProtectedRoute>
+              <StudentDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/teacher"
+          element={
+            <ProtectedRoute>
+              <TeacherDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/register-face"
+          element={
+            <ProtectedRoute>
+              <RegisterFace />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/change-password"
+          element={
+            <ProtectedRoute>
+              <ChangePassword />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/add-student"
+          element={
+            <ProtectedRoute>
+              <AddStudent />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/teacher/profile"
+          element={
+            <ProtectedRoute>
+              <EditTeacherProfile />
+            </ProtectedRoute>
+          }
+        />
+
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:uid/:token" element={<ResetPassword />} />
+
       </Routes>
     </BrowserRouter>
   );
